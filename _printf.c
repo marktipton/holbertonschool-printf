4226a5ad10ba3447a0e6f4cc03b8d8a45bb99016
marktipton <6675@holbertonstudent.com>
2023-07-16 19:56:56 -0700

    made comments

--- _printf.c
+++ _printf.c
@@ -8,16 +8,18 @@
 
 int _printf(const char *format, ...)
 {
-	int count;
-	va_list args;
+	int count;	/*number of characters printed*/
+	va_list args; 	/*variable argument list*/
 
 	count = 0;
 
-	if (format == NULL)
-		return (-1);
+	if (format == NULL)		/*check if format string is null*/
+		return (-1);		/*return -1 if null*/
 
 	va_start(args, format);
-	count = specifier_cmp(format, args);
+	count = specifier_cmp(format, args); /*set count to the return of the*/
+					/*specifier_cmp function when it is given*/
+					/*the format string and one of the args*/
 	va_end(args);
 
 	return (count);
--- get_spec_func.c
+++ get_spec_func.c
@@ -11,8 +11,8 @@
 int spec_char(va_list args)
 {
 	int c = va_arg(args, int);
-
-	_putchar(c);
+					
+	_putchar(c);	/*
 	return (1);
 }
 /**
@@ -55,36 +55,37 @@ int spec_string(va_list args)
 
 int spec_dec(va_list args)
 {
-	int d;
-	int count, i;
+	int d;		/*integer to be printed*/
+	int count, i;	/*character count and loop increment*/
 
-	d = va_arg(args, int);
+	d = va_arg(args, int);	/*Set d equal to variable argument input*/
 	count = 0, i = 10;
 
-	if (d < 0)
-	{
-		_putchar('-');
+	if (d < 0)	/*If number is negative then print a '%' and flip*/
+	{			/*the sign of the number so that it can be*/
+		_putchar('-');	/*printed normally*/
 		count++;
 		d = -d;
 	}
-	while (d / i >= 10)
-	{
-		i = i * 10;
+	while (d / i >= 10)	/*Find out how many digits the input number*/
+	{			/*is by multiplying i by 10 until d*/
+		i = i * 10;	/*divided by i is less than 10*/
 	}
 	while (i > 0)
-	{
-		if (d < 10)
-		{
+	{				
+		if (d < 10)	/*If d is less than 10 then print d, add*/
+		{		/*one to the count and return count*/
 			count += _putchar(d + '0');
 			return (count);
-		}
+		}		
+			/*Otherwise,	
 		count += _putchar(((d / i) % 10) + '0');
 		i = i / 10;
 	}
 	return (count);
 }
 /**
- * get_spec_func - selects function to perform
+ * get_spec_func - selects function to perform based on input format specifier
  *
  * @format: specifier passed as argument to _printf
  * @args: variable arguments passed to _printf function
@@ -93,7 +94,7 @@ int spec_dec(va_list args)
 
 int get_spec_func(char format, va_list args)
 {
-	spec_t spec[] = {
+	spec_t spec[] = {	/*array of specifiers and corresponding functions*/
 		{"c", spec_char},
 		{"s", spec_string},
 		{"d", spec_dec},
@@ -104,33 +105,36 @@ int get_spec_func(char format, va_list args)
 
 	count = 0;
 	i = 0;
-	while (spec[i].spec)
+	while (spec[i].spec)	/*While the specifier is not NULL, iterate*/
+				/*through specifier array to check if the*/
+				/*character passed matches the format input*/
 	{
 		if (*spec[i].spec == format)
 		{
-			count = spec[i].f(args);
-		}
-		i++;
+			count = spec[i].f(args); /*If the format matches*/
+						/*then add the character count*/
+		}				/*of that string, character, */
+		i++;				/*or number.*/
 	}
 	return (count);
 }
 /**
- * spec_check_func - checks for specific specs
+ * spec_check_func - checks if format input is a format specifier
  *
  * @format: checks format specifiers
  * Return: 1 if specifier matches and 0 if not
  */
 
-int spec_check_func(char format)
-{
+int spec_check_func(char format)		/*This function returns 1 if*/
+{						/*format is a specifier*/
 	char spec[] = {'c', 's', 'd', 'i'};
 	int i = 0;
 
-	while (spec[i])
-	{
-		if (spec[i] == format)
-		return (1);
-		i++;
+	while (spec[i])			/*Iterate to check format against all*/
+	{				/*of the specifiers*/
+		if (spec[i] == format)	
+			return (1);	/*If format is a specifier then*/
+		i++;			/*Return 1*/
 	}
-	return (0);
+	return (0);		/*Return 0 if format is not a specifier*/
 }
